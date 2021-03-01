89220a5ba065cc2a8c19bebd6482c7351f25f3e2
ryandrexlerfalkenhayn@gmail.com <ryandrexlerfalkenhayn@gmail.com>
2021-03-01 22:58:06 +0700

    Updates

---
+++ precache-manifest.ff051150466cbf2274e843fb0e55d8fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91a043a68b9423c6ca836ca4ad0058e",
+    "url": "/react-challenge-amazon-clone-master/index.html"
+  },
+  {
+    "revision": "603318c73b3fd9c06025",
+    "url": "/react-challenge-amazon-clone-master/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "339dca6c1e4994402339",
+    "url": "/react-challenge-amazon-clone-master/static/js/2.167e041b.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/react-challenge-amazon-clone-master/static/js/2.167e041b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603318c73b3fd9c06025",
+    "url": "/react-challenge-amazon-clone-master/static/js/main.1d7f63dd.chunk.js"
+  },
+  {
+    "revision": "c1f8dfee0faa6d673c6e",
+    "url": "/react-challenge-amazon-clone-master/static/js/runtime-main.437ee195.js"
+  }
+]);
